37f0a2dac1b680d4f0bddee85c320d72b2899c43
iga <iga@kukielka.pl>
2025-12-12 21:34:30 +0100

    Uproszczona eliminacja Gaussa bez pivotu - pierwsza wersja

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,23 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    int rows = mat->r;
+	int cols = mat->c;
 
-		return 0;
+	for (int c = 0; c < cols - 1; c++){
+		//uproszczona wersja gdzie zakladamy pivot rozny od zera
+		double pivot = mat->data[c][c];
+
+		for (int r = c + 1; r < rows; r++){
+			double m = mat->data[r][c] / pivot;
+			for (int i = c; i < cols; i++){
+				mat->data[r][i] -= m * mat->data[c][i];
+			}
+
+			b->data[r][0] -= m * b->data[c][0];
+		}
+		
+	}
+	return 0;
 }
 
