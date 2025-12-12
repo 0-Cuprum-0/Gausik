53c9b3c5b4ef95bec6cc931d73ecf96c3c1f6bc5
iga <iga@kukielka.pl>
2025-12-12 22:15:16 +0100

    Dodanie obsługi dzielenia przez 0 w eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,6 @@
 #include "gauss.h"
-
+#include <math.h>
+#define ZERO_TOLERANCE 1e-12
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -11,7 +12,9 @@ int eliminate(Matrix *mat, Matrix *b){
 	for (int c = 0; c < cols - 1; c++){
 		//uproszczona wersja gdzie zakladamy pivot rozny od zera
 		double pivot = mat->data[c][c];
-
+		if (fabs(pivot) < ZERO_TOLERANCE){
+			return 1;
+		}
 		for (int r = c + 1; r < rows; r++){
 			double m = mat->data[r][c] / pivot;
 			for (int i = c; i < cols; i++){
