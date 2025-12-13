58b46b25a215cf24a675905e4116726fc3aa5757
iga <iga@kukielka.pl>
2025-12-13 15:03:54 +0100

    Dodano użycie funkcji pivot w eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,6 +1,5 @@
 #include "gauss.h"
-#include <math.h>
-#define ZERO_TOLERANCE 1e-12
+
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -10,13 +9,13 @@ int eliminate(Matrix *mat, Matrix *b){
 	int cols = mat->c;
 
 	for (int c = 0; c < cols - 1; c++){
-		//uproszczona wersja gdzie zakladamy pivot rozny od zera
-		double pivot = mat->data[c][c];
-		if (fabs(pivot) < ZERO_TOLERANCE){
+		
+		if (pivot(mat, b, c)){
 			return 1;
 		}
+		double pivot_element = mat->data[c][c];
 		for (int r = c + 1; r < rows; r++){
-			double m = mat->data[r][c] / pivot;
+			double m = mat->data[r][c] / pivot_element;
 			for (int i = c; i < cols; i++){
 				mat->data[r][i] -= m * mat->data[c][i];
 			}
