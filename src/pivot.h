754915ccca3acb65fe4ecd289fee876e5ab3e2ac
iga <iga@kukielka.pl>
2025-12-13 14:53:45 +0100

    Dodano funkcję pivot oraz pomocniczą funkcję zamiany wierszy

--- src/pivot.h
+++ src/pivot.h
@@ -3,6 +3,10 @@
 
 #include "mat_io.h"
 
+//Funkcja zamienia ze soba wiersze
+
+void swap_rows(int r1, int r2, Matrix *m);
+
 /*
  * Funkcja wybiera pivot - element wzgledem ktorego wykonana bedzie eliminacja
  * W razie potrzeby zamienia wiersze w A i b tak aby pivotem byl element A[c][c]
@@ -10,6 +14,6 @@
  * Zwraca 0 - pivot wybrany poprawnie
  */
 
-int pivot(Matrix *A, Matrix *b, int c);
+int pivot(Matrix *mat, Matrix *b, int c);
 
 #endif
